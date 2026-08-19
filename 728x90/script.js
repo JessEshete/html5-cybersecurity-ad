2d68248d5d4effde7ee2928406855a0332f54503
JessEshete <mandefrojessica@gmail.com>
2026-08-19 17:57:50 -0400

    Add responsive HTML5 ad formats

---
+++ 728x90/script.js
@@ -0,0 +1,5 @@
+const learnMoreButton = document.getElementById("learnMoreBtn");
+
+learnMoreButton.addEventListener("click", function () {
+    window.open("https://www.cisa.gov/secure-our-world/recognize-and-report-phishing", "_blank");
+});
