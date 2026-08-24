76c73ca8a3c7a36d7fa9bed2e241e36f31b2524b
JessEshete <mandefrojessica@gmail.com>
2026-08-24 14:53:28 -0400

    Delete 160x600/script.js

--- 160x600/script.js
+++
@@ -1,5 +0,0 @@
-const learnMoreButton = document.getElementById("learnMoreBtn");
-
-learnMoreButton.addEventListener("click", function () {
-    window.open("https://www.cisa.gov/secure-our-world/recognize-and-report-phishing", "_blank");
-});
